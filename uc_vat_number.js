40b44410582ee7f6a4134c5186f0b64bc9626680
Wim Heirman <wim@heirman.net>
2010-03-15 13:45:13 +0000

    Patch from http://drupal.org/node/645260#comment-2450248 : - cache result of succesful validations for one day - use the SOAPClient library (although only the nuSOAP client seems to work, 'SOAP Extension on PHP5' and the EU VAT validation service don't seem to want to work together) - autocomplete on VAT field (taken from the current user's previous orders) - added VAT field to admin order interface - set Ubercart conditions, which can be used by Tax rules for conditional VAT application (using ICV / intra-community rules) - VAT number and company are never required - VAT number is in its own fieldset, so the JS showing/hiding of the billing address doesn't pose problems anymore - And of course the leading 0 was removed, I also had the JS version of the countries array generated from the PHP one so there's no duplication

--- uc_vat_number.js
+++ uc_vat_number.js
@@ -1,48 +1,21 @@
-/*List of European countries (in order of the array)
-	Allemagne
-	Autriche
-	Belgique
-	Danemark
-	Espagne
-	Finlande
-	France
-	Grce
-	Irlande
-	Italie
-	Luxembourg
-	Pays-Bas
-	Portugal
-	Royaume-Uni
-	Sude
-	Chypre
-	Estonie
-	Hongrie
-	Lettonie
-	Lituanie
-	Malte
-	Pologne
-	Rpublique tchque
-	Slovaquie
-	Slovnie*/
+// $Id$
 
 $(document).ready(
   function() {
-    var european_countries = [276, 040, 056, 208, 724, 246, 250, 300, 372, 380, 442, 528, 620, 826, 752, 196, 233, 348, 428, 440, 470, 616, 203, 703, 705];
-    var vat_label =$('#billing-pane tr:last td.field-label').html();
-    $('#edit-panes-billing-billing-country').change(function () {
-      var in_europe = false;
-      for(var country_id in european_countries) {
-        if(european_countries[country_id] == $('#edit-panes-billing-billing-country').val()) {
-          in_europe = true;
-        }
-      }
-      if(in_europe == true) {
-        $('#billing-pane tr:last td.field-label').html('<span class="form-required">*</span>' + vat_label);
-       //alert(vat_label);
-        $('#billing-pane tr:last').fadeIn();
-      } else {
-        $('#billing-pane tr:last').fadeOut();
-      }
-    })
+    $('#edit-panes-billing-billing-country').change(updateVatField)
   }
 );
+
+function updateVatField() {
+  var in_europe = false;
+  for(var country_id in european_countries) {
+    if(european_countries[country_id] == $('#edit-panes-billing-billing-country').val()) {
+      in_europe = true;
+    }
+  }
+  if(in_europe == true) {
+    $('#vat_number-pane').fadeIn();
+  } else {
+    $('#vat_number-pane').fadeOut();
+  }
+}
