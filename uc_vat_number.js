d15cc0d07e6d912ef7d6527a4eed7135fe36381c
Wim Heirman <wim@heirman.net>
2011-02-08 08:33:38 +0000

    Bringing up to date with 6.x-1.x-dev

--- uc_vat_number.js
+++ uc_vat_number.js
@@ -19,3 +19,22 @@ function updateVatField() {
     $('#vat_number-pane').fadeOut();
   }
 }
+
+$(document).ready(function() {
+  $("input[name*=vat_number]").change(getTax);
+});
+
+
+function uc_vat_number__serializeOrder() {
+  var order = __serializeOrder();
+  /* $order is a PHP serialize()d object. Patch it to include the VAT number. */
+  /* Disclaimer: YIKES!!!! Post an issue in the uc_var_module queue if you have a better solution... */
+  /*   first increment the number of elements in the object */
+  var re = /^O:8:"stdClass":([0-9]+):/;
+  var numEle = order.match(re);
+  order = order.replace(re, 'O:8:"stdClass":'+(parseInt(numEle[1])+1)+':');
+  /*   now add the VAT number (inside the 'data' array) at the end of the object */
+  var b_vat =  $("input[name*=vat_number]").val() || '';
+  order = order.replace(/}$/, 's:4:"data";a:1:{s:10:"vat_number";s:' + b_vat.bytes() + ':"' + b_vat + '";}}');
+  return order;
+}
